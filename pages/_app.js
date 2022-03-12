a64896bf308139c11a25ef038e9d82d4e380d8e7
root <joaquinolivero97@gmail.com>
2022-03-12 08:16:01 +0000

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <title>Joaquin's Portfolio</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
