87a26cf0ce537a8e98127f630630dd338618700e
root <joaquinolivero97@gmail.com>
2022-03-16 20:31:53 +0000

    added projects

--- components/Home/components/Projects.jsx
+++ components/Home/components/Projects.jsx
@@ -20,7 +20,7 @@ function Projects() {
                             <div className={styles.Projects_card_left_stack}>
                                 <span>Stack:</span>
                                 <img src="https://cdn.worldvectorlogo.com/logos/postgresql.svg" alt="" />
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png" alt="" />
+                                <img src="https://assets.website-files.com/61ca3f775a79ec5f87fcf937/6202fcdee5ee8636a145a41b_1234.png" alt="" />
                                 <img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="" />
                                 <img src="https://cdn-icons-png.flaticon.com/512/5968/5968322.png" alt="" />
                             </div>
@@ -33,7 +33,7 @@ function Projects() {
                                 Notes and their children can be exported to PDF with a single click.
                                 It has a quick search bar that goes through all personal notes.
                                 It has the option to create groups and share notes in real time with other users and a
-                                real time notification system. Check the website or github repo if you'd like to know more.
+                                real time notification system. Check the website or the GitHub repo if you'd like to know more. &#128513;
                             </p>
                         </div>
                     </div>
@@ -53,13 +53,15 @@ function Projects() {
                             <div className={styles.Projects_card_left_stack}>
                                 <span>Stack:</span>
                                 <img src="https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg" alt="" />
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png" alt="" />
+                                <img src="https://assets.website-files.com/61ca3f775a79ec5f87fcf937/6202fcdee5ee8636a145a41b_1234.png" alt="" />
                                 <img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="" />
                                 <img src="https://cdn-icons-png.flaticon.com/512/5968/5968322.png" alt="" />
                             </div>
                         </div>
                         <div className={styles.Projects_card_right}>
-
+                            <p>MovieCrit is a movie review web application. It uses <a href="https://themoviedb.org">themoviedb.org</a> API to get the films data. Registered users can write reviews and receive likes. Also, users have a profile page with a photo and their latest reviews.
+                            </p>
+                            <p>Check out the GitHub repository to learn more about details or go straight to the website. &#128515;</p>
                         </div>
                     </div>
                 </div>
@@ -67,8 +69,8 @@ function Projects() {
                 {/* FOODIEMAKERS */}
                 <div className={styles.Projects_card_container} style={{ marginBottom: '30px' }}>
                     <div className={styles.Projects_urls}>
-                        <a href="https://notecloud.xyz" target="_blank">Website</a>
-                        <a href="https://github.com/JoaquinOlivero/notecloud" target="_blank">Github Repo</a>
+                        <a href="https://foodiemakers.xyz" target="_blank">Website</a>
+                        <a href="https://github.com/JoaquinOlivero/foodiemakers" target="_blank">Github Repo</a>
                     </div>
                     <div className={styles.Projects_card_content}>
                         <div className={styles.Projects_card_left}>
@@ -78,12 +80,15 @@ function Projects() {
                             <div className={styles.Projects_card_left_stack}>
                                 <span>Stack:</span>
                                 <img src="https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg" alt="" />
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png" alt="" />
+                                <img src="https://assets.website-files.com/61ca3f775a79ec5f87fcf937/6202fcdee5ee8636a145a41b_1234.png" alt="" />
                                 <img src="https://cdn-icons-png.flaticon.com/512/5968/5968322.png" alt="" />
                             </div>
                         </div>
                         <div className={styles.Projects_card_right}>
-
+                            <p>FoodieMakers is a sort of marketplace for food manufacturers. Registered users can create publications offering products. A publication contains images, a product price, a product description and a button to contact the seller. In addition, each publication has a user review and rating system.
+                                Users interested in a product can contact the seller through real-time messaging.
+                            </p>
+                            <p>If you'd like a more detailed explanation of the website features, I recommend you to visit the GitHub repo. &#128151;</p>
                         </div>
                     </div>
                 </div>
