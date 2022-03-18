9add2409fb85067af1385ab54daba33dc9b303de
root <joaquinolivero97@gmail.com>
2022-03-18 05:41:18 +0000

    added footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar/Navbar'
 import Home from '../components/Home/Home'
 import SideNavbar from '../components/Navbar/SideNavbar'
+import Footer from '../components/Footer/Footer'
 
 export default function Index() {
   return (
@@ -8,6 +9,7 @@ export default function Index() {
       <Navbar />
       <SideNavbar />
       <Home />
+      <Footer />
     </div>
   )
 }
