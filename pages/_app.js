22d1a0cf4be39cedac728a70d988251dbaf13397
JoaquinOlivero <joaquinolivero97@gmail.com>
2022-03-19 13:14:01 +0000

    added fonts

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,10 @@ import '../styles/globals.scss'
 function MyApp({ Component, pageProps }) {
   return <>
     <Head>
+        <link rel='preload' href='/fonts/Universo/universo-regular.otf' as='font' crossOrigin='' type='font/otf'/>
+        <link rel='preload' href='/fonts/Universo/universo-bold.otf' as='font' crossOrigin='' type='font/otf'/>
+        <link rel='preload' href='/fonts/Universo/universo-black.otf' as='font' crossOrigin='' type='font/otf'/>
+        <link rel='preload' href='/fonts/Universo/universo-light.otf' as='font' crossOrigin='' type='font/otf'/>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <title>Joaquin's Portfolio</title>
     </Head>
