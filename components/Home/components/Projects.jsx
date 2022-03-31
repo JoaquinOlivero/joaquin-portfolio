8b3849a2848a36ac3b40195b2840e0c816eb31c4
JoaquinOlivero <joaquinolivero97@gmail.com>
2022-03-31 02:32:05 +0000

    improved projects

--- components/Home/components/Projects.jsx
+++ components/Home/components/Projects.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react';
 import styles from '../../../styles/Home/components/Projects.module.scss'
+import Image from 'next/image';
 
 function Projects() {
     const projectsRef = useRef(null)
@@ -26,71 +27,60 @@ function Projects() {
         <div className={styles.Projects} id='projects' ref={projectsRef}>
             <h2>Projects</h2>
             <div className={styles.Projects_cards_container}>
-
                 {/* NOTECLOUD */}
                 <div className={styles.Projects_card_container}>
-
                     <div className={styles.Projects_card_content}>
                         <div className={styles.Projects_card_left}>
-                            <div className={styles.Projects_urls}>
-                                <a href="https://notecloud.xyz" target="_blank" rel="noopener noreferrer">Website</a>
-                                <a href="https://github.com/JoaquinOlivero/notecloud" target="_blank" rel="noopener noreferrer">Github Repo</a>
-                            </div>
+
                             <div className={styles.Projects_card_left_title}>
                                 NoteCloud
                             </div>
                             <div className={styles.Projects_card_left_stack}>
-                                <span>Stack:</span>
-                                <img src="https://cdn.worldvectorlogo.com/logos/postgresql.svg" alt="" />
-                                <img src="https://assets.website-files.com/61ca3f775a79ec5f87fcf937/6202fcdee5ee8636a145a41b_1234.png" alt="" />
-                                <img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="" />
-                                <img src="https://cdn-icons-png.flaticon.com/512/5968/5968322.png" alt="" />
+                                Node - React - Express - PostgreSQL
+                            </div>
+                            <div className={styles.Projects_card_left_paragraph}>
+                                Real-time note sharing web application.
+                            </div>
+                            <div className={styles.Projects_card_left_btns} id={styles.Projects_card_left_btns_notecloud}>
+                                <a href="https://notecloud.xyz" target="_blank" rel="noopener noreferrer">Website</a>
+                                <a href="https://github.com/JoaquinOlivero/notecloud" target="_blank" rel="noopener noreferrer">GitHub</a>
                             </div>
                         </div>
-                        <div className={styles.Projects_card_right}>
-                            <p>NoteCloud is my favourite project. It is a note taking web application inspired on <a href="https://notion.so" target="_blank" rel="noopener noreferrer">notion.so</a>. It uses <a href='https://editorjs.io/' target="_blank" rel="noopener noreferrer">Editor.js</a> which is a Block-Styled editor.
-                                Once you login, it has many features, for example:
-                            </p>
-                            <p>It shows all the created notes and their children in a nested tree view.
-                                Notes and their children can be exported to PDF with a single click.
-                                It has a quick search bar that goes through all personal notes.
-                                It has the option to create groups and share notes in real time with other users and a
-                                real time notification system. Check the website or the GitHub repo if you'd like to know more. &#128513;
-                            </p>
+                        <div className={styles.Projects_card_right} id={styles.notecloud_card_right_background}>
+                            <div className={styles.Projects_card_right_image_container}>
+                                <Image src='/images/notecloud-projects_rixl6i.png' layout='fill' objectFit='cover' objectPosition="0% 20%" />
+                            </div>
                         </div>
                     </div>
                 </div>
-
                 {/* MOVIECRIT */}
-                <div className={styles.Projects_card_container}>
-
+                <div className={styles.Projects_card_container} >
                     <div className={styles.Projects_card_content}>
                         <div className={styles.Projects_card_left}>
-                            <div className={styles.Projects_urls}>
-                                <a href="https://moviecrit.xyz" target="_blank" rel="noopener noreferrer">Website</a>
-                                <a href="https://github.com/JoaquinOlivero/moviecrit" target="_blank" rel="noopener noreferrer">Github Repo</a>
-                            </div>
+
                             <div className={styles.Projects_card_left_title}>
                                 MovieCrit
                             </div>
                             <div className={styles.Projects_card_left_stack}>
-                                <span>Stack:</span>
-                                <img src="https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg" alt="" />
-                                <img src="https://assets.website-files.com/61ca3f775a79ec5f87fcf937/6202fcdee5ee8636a145a41b_1234.png" alt="" />
-                                <img src="https://cdn.worldvectorlogo.com/logos/react-2.svg" alt="" />
-                                <img src="https://cdn-icons-png.flaticon.com/512/5968/5968322.png" alt="" />
+                                Node - React - Express - MongoDB
+                            </div>
+                            <div className={styles.Projects_card_left_paragraph}>
+                                Movie review website.
+                            </div>
+                            <div className={styles.Projects_card_left_btns} id={styles.Projects_card_left_btns_moviecrit}>
+                                <a href="https://moviecrit.xyz" target="_blank" rel="noopener noreferrer">Website</a>
+                                <a href="https://gitHub.com/JoaquinOlivero/moviecrit" target="_blank" rel="noopener noreferrer">GitHub</a>
                             </div>
                         </div>
-                        <div className={styles.Projects_card_right}>
-                            <p>MovieCrit is a movie review web application. It uses <a href="https://themoviedb.org">themoviedb.org</a> API to get the films data. Registered users can write reviews and receive likes. Also, users have a profile page with a photo and their latest reviews.
-                            </p>
-                            <p>Check out the GitHub repository to learn more about details or go straight to the website. &#128515;</p>
+                        <div className={styles.Projects_card_right} id={styles.moviecrit_card_right_background}>
+                            <div className={styles.Projects_card_right_image_container}>
+                                <Image src='/images/moviecrit-projects_pqbjvk.png' layout='fill' objectFit='cover' objectPosition="0% 0%" unoptimized={true} />
+                            </div>
                         </div>
                     </div>
                 </div>
-
                 {/* FOODIEMAKERS */}
-                <div className={styles.Projects_card_container} style={{ marginBottom: '30px' }}>
+                {/* <div className={styles.Projects_card_container} style={{ marginBottom: '30px' }}>
 
                     <div className={styles.Projects_card_content}>
                         <div className={styles.Projects_card_left}>
@@ -115,9 +105,58 @@ function Projects() {
                             <p>If you'd like a more detailed explanation of the website features, I recommend you to visit the GitHub repo. &#128151;</p>
                         </div>
                     </div>
+                </div> */}
+            </div>
+            <div className={styles.Projects_extra}>
+                <div className={styles.Projects_current_work_container}>
+                    <h3>Currently working on</h3>
+                    <div className={styles.Projects_extra_cards}>
+                        <div className={styles.Projects_extra_card_container}>
+                            <div className={styles.Projects_extra_card_content}>
+                                <h4>codePDF</h4>
+                                <span>Node - Next.js - Express - PostgreSQL</span>
+                                <p>Create and style PDF documents with code.</p>
+                                <div className={styles.Projects_extra_card_content_btns}>
+                                    <a href="#" target="_blank" rel="noopener noreferrer" style={{ pointerEvents: 'none' }}>Website</a>
+                                    <a href="#" target="_blank" rel="noopener noreferrer" style={{ pointerEvents: 'none' }}>GitHub</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
+                <div className={styles.Projects_other_projects_container}>
+                    <h3>Other projects</h3>
+                    <div className={styles.Projects_extra_cards}>
+
+                        {/* FoodieMakers */}
+                        <div className={styles.Projects_extra_card_container}>
+                            <div className={styles.Projects_extra_card_content}>
+                                <h4>FoodieMakers</h4>
+                                <span>Node - EJS - Express - MongoDB</span>
+                                <p>Marketplace for food manufacturers.</p>
+                                <div className={styles.Projects_extra_card_content_btns}>
+                                    <a href="https://foodiemakers.xyz" target="_blank" rel="noopener noreferrer">Website</a>
+                                    <a href="https://github.com/JoaquinOlivero/foodiemakers" target="_blank" rel="noopener noreferrer">GitHub</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Minecraft Launcher */}
+                        <div className={styles.Projects_extra_card_container}>
+                            <div className={styles.Projects_extra_card_content}>
+                                <h4>Minecraft Launcher</h4>
+                                <span>Electron.js</span>
+                                <p>Personal Minecraft server launcher for Google Cloud Platform VM.</p>
+                                <div className={styles.Projects_extra_card_content_btns}>
+                                    <a href="https://github.com/JoaquinOlivero/gcpvm-minecraft-launcher" target="_blank" rel="noopener noreferrer">GitHub</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
