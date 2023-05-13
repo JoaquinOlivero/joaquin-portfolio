d2b63ecd67a31531aa4962fbb516bcd8b18a1a3e
JoaquinOlivero <joaquinolivero97@gmail.com>
2023-05-13 09:14:49 +0000

    added sass options

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -5,6 +7,10 @@ const nextConfig = {
     domains: ['cdn-icons-png.flaticon.com', "e7.pngegg.com", "w7.pngwing.com", "cdn.worldvectorlogo.com", "caddyserver.com", "static-00.iconduck.com", "1000logos.net"],
     unoptimized: true
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, './styles')],
+  },
+  output: "export"
 }
 
 module.exports = nextConfig
