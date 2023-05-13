756d681014884795877000a54c8c470cc8a41d50
JoaquinOlivero <joaquinolivero97@gmail.com>
2023-05-13 01:28:39 +0000

    change img to Image component

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import styles from '../../styles/Footer/Footer.module.scss'
+import Image from 'next/image'
 
 function Footer() {
     return (
@@ -11,8 +12,12 @@ function Footer() {
                 </div>
 
                 <div className={styles.Footer_content_social}>
-                    <Link href="https://github.com/JoaquinOlivero" target='_blank' rel="noopener noreferrer"><img src="https://cdn.worldvectorlogo.com/logos/github-icon-1.svg" alt="" /></Link>
-                    <Link href="https://www.linkedin.com/in/joaquin-olivero-32608a235/" target='_blank' rel="noopener noreferrer"><img src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="" /></Link>
+                    <Link href="https://github.com/JoaquinOlivero" target='_blank' rel="noopener noreferrer">
+                        <Image width={40} height={40} src="https://cdn.worldvectorlogo.com/logos/github-icon-1.svg" alt="" />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/joaquin-olivero-32608a235/" target='_blank' rel="noopener noreferrer">
+                        <Image width={35} height={40} src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="" />
+                    </Link>
                 </div>
 
             </div>
