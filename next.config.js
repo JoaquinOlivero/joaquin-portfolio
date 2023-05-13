e06219ef406be3dbf5a05526ef8239267bfbf207
JoaquinOlivero <joaquinolivero97@gmail.com>
2023-05-13 05:15:23 +0000

    added export command

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn-icons-png.flaticon.com', "e7.pngegg.com", "w7.pngwing.com", "cdn.worldvectorlogo.com", "caddyserver.com", "static-00.iconduck.com", "1000logos.net"],
+    unoptimized: true
   },
 }
 
