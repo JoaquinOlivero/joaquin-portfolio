b28605396c9d417d0a4cd29a03988b18e5008fad
JoaquinOlivero <joaquinolivero97@gmail.com>
2023-05-14 09:27:04 +0000

    updated current project

--- components/Home/components/Projects.tsx
+++ components/Home/components/Projects.tsx
@@ -1,12 +1,23 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import styles from '../../../styles/Home/components/Projects.module.scss'
 import CardsCarrousel from '../../Utils/CardsCarrousel';
+import Card from '../../Utils/Card';
 
 function Projects() {
     const projectsRef = useRef<HTMLDivElement>(null)
+    const [streamUrl, setStreamUrl] = useState<string | null>(null)
 
     // Observers
     useEffect(() => {
+        // check environment mode to set streamUrl
+        if (process.env.NODE_ENV == "development") {
+            setStreamUrl('https://player.twitch.tv/?channel=beats10&parent=portfolio.joaquinolivero.com&muted=true')
+        }
+
+        if (process.env.NODE_ENV == "production") {
+            setStreamUrl('https://player.twitch.tv/?channel=beats10&parent=joaquinolivero.com&muted=true')
+        }
+
         // Main project page observer for the right sideNavbar.
         const projectsObserver = new IntersectionObserver(entries => {
             entries.forEach(entry => {
@@ -32,8 +43,16 @@ function Projects() {
         <div className={styles.Projects} id='projects' ref={projectsRef}>
             <h2>Projects</h2>
             <CardsCarrousel />
+            <h2>Currently Working On</h2>
             <div className={styles.Projects_extra}>
-                <h2>Currently Working On</h2>
+                <Card
+                    name='Twitcher'
+                    description='Automated 24/7 video and audio Twitch streaming CLI based tool.'
+                    languages={["Go", "FFmpeg"]}
+                    github='JoaquinOlivero/Twitcher'
+                    colors={["#00ADD8", "#00ADD8"]}
+                    video={streamUrl}
+                />
             </div>
 
         </div>
